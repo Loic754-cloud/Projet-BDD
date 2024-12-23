60bcdc9efb39ee0cd2ccab7ba6d983f4cc925cb9
Loic754-cloud <loic.marliot@outlook.fr>
2024-12-23 12:45:04 +0100

    Update details_tache.php

--- details_tache.php
+++ details_tache.php
@@ -2,12 +2,12 @@
 <html>
 <head>
     <title>Détails de la tâche</title>
-    <link rel="stylesheet" type="text/css" href="../style0.css">
+    <link rel="stylesheet" type="text/css" href="style0.css">
 </head>
 
 <body>
     <?php
-    include("../connexion.php");
+    include("connexion.php");
     $con = connect();
     if (!$con) {
         echo "Problème de connexion à la base";
